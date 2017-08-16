599edcd923c201dcb32bbe2bcc9131f8c6518974
Rebekka Haley <rebekka.haley@gmail.com>
2017-08-16 11:37:02 +0200

    Added modals for sign up and log in (in navbar) and had to edit devise/registration/new and user.rb and pages + application_controller.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
   has_many :labs
   has_many :posts
   has_many :reviews
-  validates :first_name, :last_name, :password, :education, :research_interests, presence: true
+  validates :first_name, :last_name, :email, :password, presence: true
   validates :email, uniqueness: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
