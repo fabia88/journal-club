ab106c775782cbcdb2f4883bd914d5ff2c4930d3
Nathaneil Ward <nathaneilw@gmail.com>
2017-08-18 15:46:51 +0200

    working on user becoming an author for a paper

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,4 +1,9 @@
 class AuthorsController < ApplicationController
   def confirm_authorship
+    # @user = User.find(params[:id])
+    # @author =
+   return true if Author.find_by_user_id(current_user.id)
   end
 end
+
+# User can click on a link/button to toggle ownership of paper on
