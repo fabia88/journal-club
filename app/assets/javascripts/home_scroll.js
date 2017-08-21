e4db0c85ca52e479c3905ab6882eb8e70b182923
Rebekka Haley <rebekka.haley@gmail.com>
2017-08-21 18:23:12 +0200

    Changed around the bootstrap variables and config colours, changed the navbar function in the home page, changes the font-sizes in messages and nav bar to be larger and more readable

---
+++ app/assets/javascripts/home_scroll.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+
+  $(window).scroll(function () {
+    //if you hard code, then use console
+    //.log to determine when you want the
+    //nav bar to stick.
+    console.log($(window).scrollTop())
+    if ($(window).scrollTop() > 708) {
+      $('.pages-home .navbar-journal').addClass('navbar-opaque');
+    }
+    if ($(window).scrollTop() < 709) {
+      $('.pages-home .navbar-journal').removeClass('navbar-opaque');
+    }
+  });
+});
