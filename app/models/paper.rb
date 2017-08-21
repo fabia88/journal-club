307c196127c38f8cbef270c8eb5140ac25d981f7
Fabia Moroni <fabia.moroni@gmail.com>
2017-08-21 11:33:01 +0200

    added dependent on papers posts

--- app/models/paper.rb
+++ app/models/paper.rb
@@ -2,6 +2,6 @@ class Paper < ApplicationRecord
   has_many :reviews, dependent: :destroy
   has_many :paper_authors
   has_many :authors, through: :paper_authors
-  has_many :posts
+  has_many :posts, dependent: :destroy
   # validates :title, :source, :reference, presence: true
 end
