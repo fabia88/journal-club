3c33fd50ae2138f97f82a87e9d39a6afb38a736b
Nathaneil Ward <nathaneilw@gmail.com>
2017-08-21 15:37:41 +0200

    First step works, we can link the current_user to its existing author, through a link on the paper show view

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,11 +1,10 @@
 class Author < ApplicationRecord
   belongs_to :user, optional: true
+  has_many :paper_authors
   has_many :papers, through: :paper_authors
   validates :name, presence: true
 
   def self.confirm_authorship(current_user)
-    # @user = User.find(params[:id])
-    # @author =
    Author.find_by_user_id(current_user.id) ? true : false
   end
 end
--- app/models/paper.rb
+++ app/models/paper.rb
@@ -4,4 +4,9 @@ class Paper < ApplicationRecord
   has_many :authors, through: :paper_authors
   has_many :posts
   # validates :title, :source, :reference, presence: true
+
+  def is_user_potential_author?(user, author)
+    user_author_name = "#{user.last_name} #{user.first_name[0]}"
+    author.name == user_author_name
+  end
 end
