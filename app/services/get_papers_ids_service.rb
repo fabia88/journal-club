6b0d57f656de9aa010fdab6470e8d3a184aacd33
Fabia Moroni <fabia.moroni@gmail.com>
2017-08-23 23:23:42 +0200

    increased n of papers

--- app/services/get_papers_ids_service.rb
+++ app/services/get_papers_ids_service.rb
@@ -1,12 +1,12 @@
 class GetPapersIdsService
   require "open-uri"
   require "json"
-  def initialize(number_of_papers = 3000)
+  def initialize(number_of_papers = 6000)
     @number_of_papers = number_of_papers
   end
 
   def call
-    url = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pubmed&term=hasabstract&sort=pub+date&reldate=3&datetype=pdat&retmax=#{@number_of_papers}&retmode=json&tool=journal_link&email=fabia.moroni@gmail.com"
+    url = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pubmed&term=hasabstract&sort=pub+date&reldate=5&datetype=pdat&retmax=#{@number_of_papers}&retmode=json&tool=journal_link&email=fabia.moroni@gmail.com"
     JSON.parse(open(url).read)["esearchresult"]["idlist"]
   end
 end
--- app/services/import_new_papers_service.rb
+++ app/services/import_new_papers_service.rb
@@ -1,5 +1,5 @@
 class ImportNewPapersService
-  def initialize(number_of_papers = 3000)
+  def initialize(number_of_papers = 6000)
     @number_of_papers = number_of_papers
   end
 
